d8bc98eff2aeceaac4d01a7aae29ba09b63a06ad
Matous <bralor92@email.cz>
2022-01-07 13:57:10 +0100

    test_captcha_generator.py: test suit completed

--- captcha_solver/test/test_captcha_generator.py
+++ captcha_solver/test/test_captcha_generator.py
@@ -11,20 +11,38 @@ class TestCaptchaContentGenerator:
         uppercase = True
         self.test = CaptchaContentGenerator(numbers, lowercase, uppercase)
 
-    def test_generator_with_correct_parameter_numbers(self):
+    def test_correct_parameter_numbers(self):
         assert self.test.numbers
 
-    def test_generator_with_incorrect_parameter_numbers(self):
+    def test_incorrect_parameter_numbers(self):
         assert self.test.numbers is not False
 
-    def test_generator_with_correct_parameter_lowercase(self):
+    def test_correct_parameter_lowercase(self):
         assert self.test.lowercase
 
-    def test_generator_with_incorrect_parameter_lowercase(self):
+    def test_incorrect_parameter_lowercase(self):
         assert self.test.lowercase is not False
 
-    def test_generator_that_collects_correct_numbers(self):
+    def test_collecting_of_correct_numbers(self):
         assert self.test.collect_numbers() == "0123456789"
 
-    def test_generator_that_collects_incorrect_numbers(self):
+    def test_collecting_of_incorrect_numbers(self):
         assert self.test.collect_numbers() != "012346789"
+
+    def test_collecting_of_correct_lowercase_letters(self):
+        assert self.test.collect_lowercase() == 'abcdefghijklmnopqrstuvwxyz'
+
+    def test_collecting_of_incorrect_lowercase_letters(self):
+        assert self.test.collect_lowercase() != 'abcdfghijklmnopqrstuvwxyz'
+
+    def test_collecting_of_correct_uppercase_letters(self):
+        assert self.test.collect_uppercase() == 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    def test_collecting_of_incorrect_uppercase_letters(self):
+        assert self.test.collect_uppercase() != 'ABCDEFGHIJKLMOPQRSTUVWXYZ'
+
+    def test_creating_correct_data_type_of_captcha(self):
+        assert isinstance(self.test.create_content(), str)
+
+    def test_creating_captchas_content_of_correct_length(self):
+        assert len(self.test.create_content()) == 5
